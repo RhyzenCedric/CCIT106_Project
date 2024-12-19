4ada4ea47382d7608311379e36a7aaf86fcc8b44
RhyzenCedric <rcedricgarcia@gmail.com>
2024-12-19 23:36:31 +0800

    location cards added 2

--- clinnect/src/App.jsx
+++ clinnect/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Register from './components/Register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
 
 
@@ -10,7 +9,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/landingpage" element={<LandingPage/>} />
           <Route path="/register" element={<Register/>} />
         </Routes>
       </Router>
